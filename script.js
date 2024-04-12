27de5c85fb3c6bc12a23273bb3ae0cb16b7d2f67
vemkaze <abhishekverma654321@gmail.com>
2024-04-13 00:17:53 +0530

    modified script.js

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const SNAKE_COLOUR = 'lightgreen';
 const SNAKE_BORDER_COLOUR = 'darkgreen';
 const FOOD_COLOUR = 'red';
 const FOOD_BORDER_COLOUR = 'darkred';
+
+let snake = [
+    {x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150}
+  ]
+
+let score = 0;
+
+let changingDirection = false;
+
+let foodX;
+
+let foodY;
+
+let vx = 10;
+
+let vy = 0;
+
+const canvas = document.getElementById('canvas');
+
+ 
