db89673344a7dd84240f9c32887767dd17c1ff53
vemkaze <abhishekverma654321@gmail.com>
2024-04-12 23:54:05 +0530

    modified html file and script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const GAME_SPEED = 100;
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = 'blue';
+const SNAKE_COLOUR = 'lightgreen';
+const SNAKE_BORDER_COLOUR = 'darkgreen';
+const FOOD_COLOUR = 'red';
+const FOOD_BORDER_COLOUR = 'darkred';
