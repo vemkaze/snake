453fdcb02bb8db6c7b4f431a3917aed5b218f3c2
vemkaze <abhishekverma654321@gmail.com>
2024-04-13 23:17:35 +0530

    fixed issues regarding names of elements

--- script.js
+++ script.js
@@ -26,8 +26,21 @@ let vx = 10;
 
 let vy = 0;
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('Canvas');
 
-const ctx = gameCanvas.getContext("2d");
+const ctx = Canvas.getContext("2d");
 
- 
+blankCanvas();
+
+function blankCanvas() {
+  
+  ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+
+  ctx.strokestyle = CANVAS_BORDER_COLOUR;
+
+  
+  ctx.fillRect(0, 0, Canvas.width, Canvas.height);
+  
+  ctx.strokeRect(0, 0, Canvas.width, Canvas.height);
+
+}
