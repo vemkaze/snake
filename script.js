8d5260cbd60a7a6bd92f9c314f417e5b96845cba
vemkaze <abhishekverma654321@gmail.com>
2024-04-18 00:39:46 +0530

    modified js file

--- script.js
+++ script.js
@@ -1,85 +1,24 @@
-const GAME_SPEED = 100;
-const CANVAS_BORDER_COLOUR = 'black';
-const CANVAS_BACKGROUND_COLOUR = 'blue';
-const SNAKE_COLOUR = 'lightgreen';
-const SNAKE_BORDER_COLOUR = 'darkgreen';
-const FOOD_COLOUR = 'red';
-const FOOD_BORDER_COLOUR = 'darkred';
-
-let snake = [
-    {x: 150, y: 150},
-    {x: 140, y: 150},
-    {x: 130, y: 150},
-    {x: 120, y: 150},
-    {x: 110, y: 150}
-  ]
-
-let score = 0;
-
-let changingDirection = false;
-
+const gameBoard = document.getElementById("gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.getElementById("scoreText");
+const resetBtn = document.getElementById("resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "yellow";
+const snakeBorder = "black";
+const foodColor = "green0";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
 let foodX;
-
 let foodY;
-
-let vx = 10;
-
-let vy = 0;
-
-const canvas = document.getElementById('Canvas');
-
-const ctx = Canvas.getContext("2d");
-
-blankCanvas();
-
-function blankCanvas() {
-  
-  ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
-
-  ctx.strokestyle = CANVAS_BORDER_COLOUR;
-
-  
-  ctx.fillRect(0, 0, Canvas.width, Canvas.height);
-  
-  ctx.strokeRect(0, 0, Canvas.width, Canvas.height);
-
-}
-
-function drawFood() {
-  ctx.fillStyle = FOOD_COLOUR;
-  ctx.strokestyle = FOOD_BORDER_COLOUR;
-  ctx.fillRect(foodX, foodY, 10, 10); 
-  ctx.strokeRect(foodX, foodY, 10, 10);
-}
-
-function advanceSnake() {
-  const head = {x: snake[0].x === foodX && snake[0].y === foodY};
-  if (didEatFood){
-    score = score + 10;
-    document.getElementById('score').innerHTML = score;
-    
-  createFood();
-  } else {
-    snake.pop();
-  }
-}
-
-function randomTen(min, max) {
-  return Math.round((Math.random() * (max-min) + min) / 10) * 10;
-}
-
-function createFood() {
-
-  foodX = randomTen(0, gameCanvas.width - 10);
-  
-  foodY = randomTen(0, gameCanvas.height - 10);
-
-  
-  snake.forEach(function isFoodOnSnake(part) {
-    const foodIsoNsnake = part.x == foodX && part.y == foodY;
-    if (foodIsoNsnake) createFood();
-  });
-}
-
-
-
+let score = 0;
+let snake = [
+  {x:unitSize*4 , Y:0},
+  {x:unitSize*3 , y:0},
+  {X:unitSize*2 , y:0},
+  {x:unitSize,y:0},
+  {X:0, y:0}
+]
