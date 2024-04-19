4381adb039267cd475355b196cb47c0ad7f5ecd1
vemkaze <abhishekverma654321@gmail.com>
2024-04-19 20:47:48 +0530

    modified script.js

--- script.js
+++ script.js
@@ -114,7 +114,63 @@ function changeDirection(event){
     yVelocity = unitSize;
   }
 };
-function checkGameOver(){};
-function displayGameOver(){};
-function resetGame(){};
+function checkGameOver(){
+  switch(true){
+    case (snake[0].x <0):
+      running = false;
+      break;
+    case (snake[0].x >= gameWidth):
+      running = false;
+      break;
+    case  (snake[0].y <0):
+      running = false;
+      break;
+    case (snake[0].y >= gameHeight):
+      running = false;
+      break; 
+  }
+  for(let i = 1; i <snake.length; i++){
+    if(snake[i].x == snake[0].x && snake[i].y == snake[0].y){
+      running = false;
+    }
+  }
+};
+
+function displayGameOver(){
+
+  ctx.font = "50px New Times Roman";
+
+  ctx.fillStyle = "black";
+
+  ctx.textAlign = "center";
+
+  ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
+
+  running = false;
+
+};
+
+
+function resetGame(){
+  score = 0;
+
+  xVelocity = unitSize;
+
+  yVelocity = 0;
+
+  snake = [
+
+      {x:unitSize * 4, y:0},
+
+      {x:unitSize * 3, y:0},
+
+      {x:unitSize * 2, y:0},
+
+      {x:unitSize, y:0},
+
+      {x:0, y:0}
+
+  ];
+  gameStart();
+};
 
